d0c5f17a2afe74f12e62a8ec80a72f6809f9cfb6
Alvaro Garcia <derecerca@gmail.com>
2014-12-10 20:28:25 +0100

    refactor: remove duplicated test (included in the other one)

--- src/test/java/com/example/kata/testmockito/InvoiceTest.java
+++ src/test/java/com/example/kata/testmockito/InvoiceTest.java
@@ -2,7 +2,6 @@ package com.example.kata.testmockito;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,11 +14,6 @@ public class InvoiceTest {
 	@Before
 	public void setUp() {
 		sut = new Invoice();
-	}
-
-	@Test
-	public void an_invoice_should_exist() {
-		assertThat(sut, not(equalTo(null)));
 	}
 
 	@Test
