379e0862f4b459851d743da7376ef2720d04054e
Alvaro Garcia <derecerca@gmail.com>
2014-12-10 20:27:57 +0100

    refactor: DRY - remove duplication of sut

--- src/test/java/com/example/kata/testmockito/InvoiceTest.java
+++ src/test/java/com/example/kata/testmockito/InvoiceTest.java
@@ -4,19 +4,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class InvoiceTest {
 
+	private static Invoice sut = new Invoice();
+
+	@Before
+	public void setUp() {
+		sut = new Invoice();
+	}
+
 	@Test
 	public void an_invoice_should_exist() {
-		assertThat(new Invoice(), not(equalTo(null)));
+		assertThat(sut, not(equalTo(null)));
 	}
 
 	@Test
 	public void an_empty_invoice_should__total_as_zero() {
-		assertThat(new Invoice().getTotal(), equalTo(new Amount(0)));
+		assertThat(sut.getTotal(), equalTo(new Amount(0)));
 	}
 
 }
